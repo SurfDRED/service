d9a9bcce980580c7b0960ec3cf2f398ae6042ca6
Dmitrii Iachmentcev <d.yachmentsev@gmail.com>
2020-02-22 18:49:19 +0100

    chang service-tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,41 +1,63 @@
 package ru.netology.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CashbackHackServiceTest {
 
     @Test
-    void serviceShouldAdvise(){
-        // подготовка
+    void serviceShould100Advise1900(){
+        // given
         CashbackHackService service = new CashbackHackService();
         int amount = 1900;
-        // выполнение целевого действия
+        // when
         int actual = service.remain(amount);
         int expected = 100;
-        // сравнение ожидаемого и фактического
+        // then
         assertEquals(expected, actual);
     }
+
     @Test
-    void serviceShouldAdvise0Cashback(){
-        // подготовка
+    void serviceShould0Advise1000(){
+        // given
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
-        // выполнение целевого действия
+        // when
         int actual = service.remain(amount);
         int expected = 0;
-        // сравнение ожидаемого и фактического
+        // then
         assertEquals(expected, actual);
     }
+
     @Test
-    void serviceShouldAdvise0Pay(){
-        // подготовка
+    void serviceShould0Advise0(){
+        // given
         CashbackHackService service = new CashbackHackService();
         int amount = 0;
-        // выполнение целевого действия
+        // when
+        assertThrows(IllegalArgumentException.class, () ->service.remain(amount) );
+    }
+
+    @Test
+    void serviceShould0AdviseMinus(){
+        // given
+        CashbackHackService service = new CashbackHackService();
+        int amount = -1;
+        // when
+        assertThrows(IllegalArgumentException.class, () ->service.remain(amount) );
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/service.csv", numLinesToSkip = 1)
+    void serviceShouldAdvise(int amount, int expected, String message) {
+        // given
+        CashbackHackService service = new CashbackHackService();
+        // when
         int actual = service.remain(amount);
-        int expected = 1000;
-        // сравнение ожидаемого и фактического
-        assertEquals(expected, actual);
+        // then
+        assertEquals(expected, actual, message);
     }
 }
